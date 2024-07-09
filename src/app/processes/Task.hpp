a36643874c1ffb06a3cf66e5058fbab34b321ef3
Navdeep <108007267+nxvtej@users.noreply.github.com>
2024-07-09 14:59:00 +0530

    completed task serialization and deserialization

--- src/app/processes/Task.hpp
+++ src/app/processes/Task.hpp
@@ -25,5 +25,48 @@ struct Task
     // do complete task
     // movinf owndership here // && is temp buffer thus it will be moved without copy
     Task(std::fstream &&stream, Action act, std::string filePath) : f_stream(std::move(stream)), action(act), filePath(filePath) {}
+
+    // instead of directly passing task to queue
+    // serialization the task to ostringstream
+    std::string toString()
+    {
+        std::ostringstream oss;
+        oss << filePath << "," << (action == Action::ENCRYPT ? "ENCRYPT" : "DECRYPT");
+        // example.txt ENCRYPT f_stream
+        // then convert this into stream of string as
+        // "example.txt,ENCRYPT"
+        return oss.str();
+    }
+
+    // deserialize the task
+    static Task fromString(const std::string &taskData)
+    {
+        std::istringstream iss(taskData);
+        std::string filePath;
+        std::string actionStr;
+
+        // "," not this , created error as it was stirng
+        if (std::getline(iss, filePath, ',') && std::getline(iss, actionStr))
+        {
+            // getting from string stream
+            Action action = (actionStr == "ENCRYPT" ? Action::ENCRYPT : Action::DECRYPT);
+            IO io(filePath);
+            // this removes
+            std::fstream f_stream = std::move(io.getFileStream()); // not just referecnce actual
+
+            if (f_stream.is_open())
+            {
+                return Task(std::move(f_stream), action, filePath);
+            }
+            else
+            {
+                throw std::runtime_error("Failed to open file" + filePath);
+            }
+        }
+        else
+        {
+            throw std::runtime_error("Invalid task format data");
+        }
+    }
 };
 #endif
